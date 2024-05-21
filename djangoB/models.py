c314553fcd02d8b4c5c8eed548b4f380c1910c39
hidetoshi123 <ros90909@gmail.com>
2024-05-21 21:33:09 +0800

    first commit

---
+++ djangoB/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Gender(models.Model):
+    gender_id = models.BigAutoField(primary_key=True)
+    # gender_id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY - SQL EQUIVALENT
+    gender = models.CharField(max_length=55) #gender VARCHAR(55) NOT NULL - sql equivalent
+    created_at = models.DateTimeField(auto_now_add=True) # TIMESTAMP DEFAULT CURRENT TIMESTAMP
+    updated_at = models.DateTimeField(auto_now=True) # TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    class Meta:
+        db_table = 'genders'
+   
+class User (models.Model):
+    user_id = models.BigAutoField(primary_key=True, blank=False)
+    first_name = models.CharField(max_length=55, blank=False)
+    middle_name = models.CharField(max_length=55, blank=True)
+    last_name = models.CharField(max_length=55, blank=False)  
+    age = models.IntegerField(blank=False)
+    birth_date = models.DateField()
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
+    username = models.CharField(max_length=55, blank=False)
+    password = models.CharField(max_length=255, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True) # TIMESTAMP DEFAULT CURRENT TIMESTAMP
+    updated_at = models.DateTimeField(auto_now=True) # TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    class Meta:
+        db_table = 'users'
